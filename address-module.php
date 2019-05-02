34897bb607fb20928ae5acd439dee6ff810160d9
Orion55 <grol55@mail.ru>
2019-05-02 22:37:22 +0500

    first commit

---
+++ address-module.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Address Module
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Плагин сбора адресов
+Version: 1.0
+Author: Grebenev Oleg
+Author URI: http://portfolio.infoblog72.ru/
+License: GPL2
+*/
+
+defined('ABSPATH') or die('Nope, not accessing this');
+
+require plugin_dir_path(__FILE__) . 'inc/admin-panel.php';
---
+++ inc/admin-panel.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
+function crb_attach_theme_options()
+{
+    Container::make('theme_options', __('Опции плагина сбора адресов', 'crb'))
+        ->set_icon('dashicons-external')
+        ->set_page_menu_title('Сбор адресов')
+        ->set_page_menu_position(7)
+        ->add_fields(array(
+            Field::make('html', 'crb_information_text')
+                ->set_html("<div><b>[address-module]</b> - шорткод для сбора адресов</div>"),
+            Field::make('text', 'crb_email', 'Email')
+                ->set_help_text('Email для отправки заказа')
+        ));
+}
